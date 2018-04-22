2756e7dddaaec008576a4d3c5a70a10a21821724
TRIGGEREDNICK <nick05@tiscali.it>
2018-04-22 10:45:16 +0200

    Update

--- src/Encoder.php
+++ src/Encoder.php
@@ -8,17 +8,15 @@ class Encoder {
 	const INDENT = '    ';
 
 	public function __construct($array){
-		$this->array = (strpos($array, '{') === 0) ? 
-json_decode($array, true) : $array;
+		$this->array = (strpos($array, '{') === 0) ? json_decode($array, true) : $array;
 	}
 
 	public function toHER(){
 		$this->HER = NULL;
 		foreach($this->array as $arg => $value){
-			if((isset($value)) and (count($value) > 0)){
+			if(isset($value)){
 				$this->HER .= '- '.$arg.' -'.PHP_EOL;
-				foreach($value as $f => $v) $this->HER 
-.= self::INDENT.'* '.$f.' = '.$v.PHP_EOL;
+				foreach($value as $f => $v) $this->HER .= self::INDENT.'* '.$f.' = '.$v.PHP_EOL;
 			} else {
 				//$this->HER .= '* '.$arg.PHP_EOL; /* to fix */
 			}
