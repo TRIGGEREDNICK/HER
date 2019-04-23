66d895507592bfcdc2dbd385783eeb548a00ef63
TRIGGEREDNICK <nick05@tiscali.it>
2019-04-23 16:07:12 +0000

    Apply fixes from StyleCI

--- HER.php
+++ HER.php
@@ -1,34 +1,42 @@
 <?php
 
-function her_encode($array){
-	$HER = '';
-	foreach($array as $arg => $value){
-		if(isset($value)){
-			$HER .= '- '.$arg.' -'.PHP_EOL;
-			foreach ($value as $f => $v) $HER .= '  * '.$f.' = '.$v.PHP_EOL;
-		} else {
-			//$this->HER .= '* '.$arg.PHP_EOL; /* to fix */
-		}
-	}
-	return $HER;
+function her_encode($array)
+{
+    $HER = '';
+    foreach ($array as $arg => $value) {
+        if (isset($value)) {
+            $HER .= '- '.$arg.' -'.PHP_EOL;
+            foreach ($value as $f => $v) {
+                $HER .= '  * '.$f.' = '.$v.PHP_EOL;
+            }
+        } else {
+            //$this->HER .= '* '.$arg.PHP_EOL; /* to fix */
+        }
+    }
+
+    return $HER;
 }
 
-function her_decode($HER){
-	$i = 0;
-	$array = [];
-	foreach(explode(PHP_EOL, $HER) as $value){
-		$value = str_replace(' ', '', $value);
-		if(strpos($value, '#') === 0) continue;
+function her_decode($HER)
+{
+    $i = 0;
+    $array = [];
+    foreach (explode(PHP_EOL, $HER) as $value) {
+        $value = str_replace(' ', '', $value);
+        if (strpos($value, '#') === 0) {
+            continue;
+        }
 
-		if(strpos($value, '-') === 0){
-			$value = str_replace('-', '', $value);
-			$i = $value;
-		} elseif(strpos($value, '*') === 0){
-			$value = str_replace('*', '', $value);
-			$value = explode('=', $value, 2);
-			$array[$i][] = [$value[0] => $value[1]];
-		}
-		unset($value);
-	}
-	return $array;
+        if (strpos($value, '-') === 0) {
+            $value = str_replace('-', '', $value);
+            $i = $value;
+        } elseif (strpos($value, '*') === 0) {
+            $value = str_replace('*', '', $value);
+            $value = explode('=', $value, 2);
+            $array[$i][] = [$value[0] => $value[1]];
+        }
+        unset($value);
+    }
+
+    return $array;
 }
--- example.php
+++ example.php
@@ -3,14 +3,14 @@
 require_once __DIR__.'/HER.php';
 
 $array = [
-	'Category' => [
-		'Cool' => 1,
-		'ok'   => 1,
-	],
-	'Two' => [
-		'cool' => 0,
-		'ok'   => 0,
-	],
+    'Category' => [
+        'Cool' => 1,
+        'ok'   => 1,
+    ],
+    'Two' => [
+        'cool' => 0,
+        'ok'   => 0,
+    ],
 ];
 
 $encoded = her_encode($array);
