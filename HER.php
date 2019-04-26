6d5b0eca1500ebc3822ec7ed346d64e9062052f4
TRIGGEREDNICK <nick05@tiscali.it>
2019-04-26 18:07:26 +0200

    Update to the new HER version.

--- HER.php
+++ HER.php
@@ -1,42 +1,88 @@
 <?php
 
-function her_encode($array)
-{
-    $HER = '';
-    foreach ($array as $arg => $value) {
-        if (isset($value)) {
-            $HER .= '- '.$arg.' -'.PHP_EOL;
-            foreach ($value as $f => $v) {
-                $HER .= '  * '.$f.' = '.$v.PHP_EOL;
-            }
-        } else {
-            //$this->HER .= '* '.$arg.PHP_EOL; /* to fix */
-        }
-    }
-
-    return $HER;
+function her_formatter($value){
+	if($value == '1') return 'True';
+	elseif($value == '0') return 'False';
+	else return '"'.$value.'"';
 }
 
-function her_decode($HER)
-{
-    $i = 0;
-    $array = [];
-    foreach (explode(PHP_EOL, $HER) as $value) {
-        $value = str_replace(' ', '', $value);
-        if (strpos($value, '#') === 0) {
-            continue;
-        }
+function array_formatter($value){
+	if($value == 'True') return true;
+	elseif($value == 'False') return false;
+	else return substr($value, 1, -1);
+}
 
-        if (strpos($value, '-') === 0) {
-            $value = str_replace('-', '', $value);
-            $i = $value;
-        } elseif (strpos($value, '*') === 0) {
-            $value = str_replace('*', '', $value);
-            $value = explode('=', $value, 2);
-            $array[$i][] = [$value[0] => $value[1]];
-        }
-        unset($value);
-    }
+function her_encode($array){
+	$HER = '';
+	$ind = '    ';
 
-    return $array;
+	foreach($array as $dictionary => $array_dict){
+		$dictionary = ucfirst($dictionary);
+
+		$HER .= '- '.$dictionary.' -'.PHP_EOL;
+
+		$i = 0;
+		foreach($array_dict as $arg => $value){
+
+			if(is_array($value)){
+				$HER .= $ind.'>> '.ucfirst($arg).'[]'.PHP_EOL;
+				$arg = ucfirst($arg);
+				foreach($value as $v){
+					$v = her_formatter($v);
+					$HER .= $ind.'* '.$arg.'[] = '.$v.PHP_EOL;
+				}
+			} elseif(is_numeric($arg)){
+				if($i == 0) $HER .= $ind.'>> Array[]'.PHP_EOL;
+				$value = her_formatter($value);
+				$HER .= $ind.'* Array[] = '.$value.PHP_EOL;
+				$i++;
+			} else {
+				$value = her_formatter($value);
+				$HER .= $ind.' * '.$arg.' = '.$value.PHP_EOL;
+			}
+		}
+	}
+
+	return $HER;
+}
+
+function her_decode($HER){
+	$r = 0;
+	$array = [];
+	$ind = '    ';
+	$HER = explode(PHP_EOL, $HER);
+
+
+	foreach($HER as $line){
+		//$line = str_replace($ind, '', $line);
+
+		if(stripos($line, '#') === 0) continue;
+		elseif(stripos($line, '>>') === 0) continue;
+		elseif(stripos($line, '-') === 0){
+			$line = substr($line, 2, -2);
+			$line = strtolower($line);
+			$r = $line;
+		} elseif(stripos($line, '*') !== false){
+			$value = explode('=', $line, 2);
+
+			$value[0] = str_replace([' ', '*'], '', $value[0]);
+			$value[0] = strtolower($value[0]);
+			$value[1] = substr($value[1], 1);
+			$value[1] = array_formatter($value[1]);
+
+			if(stripos($value[0], '[]') !== false){
+				$value[0] = str_replace('[]', '', $value[0]);
+				if($value[0] == 'array'){
+					$array[$r][] = $value[1];
+				} else {
+					if(!is_numeric($value[0])) $array[$r][$value[0]][] = $value[1];
+					else $array[$r][] = $value[1];
+				}
+			} else {
+				$array[$r][$value[0]] = $value[1];
+			}
+		}
+	}
+
+	return $array;
 }
--- example.php
+++ example.php
@@ -3,21 +3,34 @@
 require_once __DIR__.'/HER.php';
 
 $array = [
-    'Category' => [
-        'Cool' => 1,
-        'ok'   => 1,
+    'christmas' => [
+    	'greetings' => [
+        	'Merry christmas!',
+        	'Spam, PHP, Eggs',
+        	'example' => true,
+        ]
     ],
-    'Two' => [
-        'cool' => 0,
-        'ok'   => 0,
+    'HER' => [
+    	'cool' => true,
+    	'useless' => false,
     ],
+    'XML' => [
+    	'useless',
+    	'old_style',
+    ],
+    'JSON' => [
+    	'useless',
+    	'old_style',
+    ]
 ];
 
 $encoded = her_encode($array);
 echo $encoded.PHP_EOL;
 
+
 $decoded = her_decode($encoded);
 print_r($decoded);
 
-$json = json_encode($decoded);
-echo $json;
+
+$json = json_encode($decoded, JSON_PRETTY_PRINT);
+echo $json.PHP_EOL;
